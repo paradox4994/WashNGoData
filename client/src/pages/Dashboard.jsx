a7536acc277b6ca331bee36eb8a797ca68076254
paradox4994 <ryanpereira499@gmail.com>
2024-03-09 22:32:04 -0500

    Added Sidebar for Navigation

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,34 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/userContext";
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
+// MUI Imports
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
+// MUI Icons Import
+
+import ShopRoundedIcon from '@mui/icons-material/ShopRounded';
+import LibraryAddRoundedIcon from '@mui/icons-material/LibraryAddRounded';
+import ViewListRoundedIcon from '@mui/icons-material/ViewListRounded';
+import Projects from "../components/Projects";
+import Templates from "../components/Templates";
+import TemplateStore from "../components/TemplateStore";
 
 export default function Dashboard() {
-  const { user, setUser } = useContext(UserContext);
 
-  const navigate = useNavigate()
-  
+  const { user, setUser } = useContext(UserContext);
+  const [page, setPage] = useState(1)
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (!user) {
       axios
@@ -22,14 +40,52 @@ export default function Dashboard() {
           console.error("Error fetching user profile:", error);
         });
     }
-  },[])
+  }, []);
+
+  const handlePageChange = (pageNumber) => {
+    setPage(pageNumber)
+  }
+
 
   return (
     <div>
-      <h1>Dashbaord</h1>
       {user ? (
         <>
-          <h2>Hi {user.name} </h2>
+          <Grid container sx={{pt: 1, pl: 1}}>
+            <Grid item xs={2}>
+              <List sx={{backgroundColor: "#eaeaea", borderRadius: 3, boxShadow: 1}}>
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => handlePageChange(1)}>
+                    <ListItemIcon>
+                      <LibraryAddRoundedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Projects" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => handlePageChange(2)}>
+                    <ListItemIcon>
+                      <ViewListRoundedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Templates" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => handlePageChange(3)}>
+                    <ListItemIcon>
+                      <ShopRoundedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Template Store" />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Grid>
+            <Grid item xs={10}>
+              {page === 1 && <Projects/>}
+              {page === 2 && <Templates/>}
+              {page === 3 && <TemplateStore/>}
+            </Grid>
+          </Grid>
         </>
       ) : (
         <p>Loading user data...</p>
