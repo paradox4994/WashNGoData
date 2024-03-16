73404ac7de19fa7cad6b34700cab69926aed3889
paradox4994 <ryanpereira499@gmail.com>
2024-03-16 00:47:05 -0400

    Fixed "newtemplate" route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/newTemplate' element={<NewTemplate/>}/>
+        <Route path='/newtemplate' element={<NewTemplate/>}/>
       </Routes>
     </UserContextProvider>
   )
