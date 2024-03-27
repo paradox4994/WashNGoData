3c394127ba9e57221b7a56a31e8b57804d0ae097
paradox4994 <ryanpereira499@gmail.com>
2024-03-27 13:43:00 -0400

    Add Page

--- client/src/components/TableComp.jsx
+++ client/src/components/TableComp.jsx
@@ -1,13 +1,20 @@
 import {React, useState} from "react";
 import { useReactTable } from "@tanstack/react-table";
 
+const columns = [
+  {
+     ascessorKey: 'task',
+     header: "task",
+  }
+]
+
 export default function TableComp() {
 
-    const [data,setData] = useState(DATA)
+    const [data,setData] = useState()
 
     const table = useReactTable({
-        data,
-        columns,
+      data,
+      columns,
     })
   return <div></div>;
 }
