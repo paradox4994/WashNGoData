2f93073b34db319223b7b79ad7b00853384c98c1
paradox4994 <ryanpereira499@gmail.com>
2024-03-30 01:23:36 -0400

    Add middleware

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const cors = require('cors')
-const {projects, saveProjects, viewProjects} = require('../controllers/projectControllers')
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const upload = require('../helpers/multerConfig'); 
+const {projects, saveProjects, viewProjects, deleteProjects} = require('../controllers/projectControllers');
 
-
-//Middleware
+// Middleware
 router.use(
     cors({
         credentials: true,
         origin: "http://localhost:5173"
     })
-)
+);
 
-router.post('/', projects)
-router.post('/viewproject', viewProjects)
-router.post('/saveproject', saveProjects)
 
-module.exports = router
+router.post('/', projects);
+router.post('/viewproject', viewProjects);
+router.post('/saveproject', upload.single('file'), saveProjects);
+router.post('/deleteproject', deleteProjects);
+
+module.exports = router;
