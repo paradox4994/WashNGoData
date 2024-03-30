d6569af4792f5472e6b81bc32a3746192768862f
paradox4994 <ryanpereira499@gmail.com>
2024-03-30 01:11:41 -0400

    Add Table Component

--- client/src/components/ProjectTable.jsx
+++ client/src/components/ProjectTable.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TableComp from './TableComp'
 
 export default function ProjectTable() {
   return (
     <div>
-      Project Table
+      <TableComp/>
     </div>
   )
 }
