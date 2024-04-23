cfb851bcdd6292e2a1975d63e3bedbffb57cba14
paradox4994 <ryanpereira499@gmail.com>
2024-04-23 14:58:09 -0400

    a

--- client/src/components/charts/LineChartComp.jsx
+++ client/src/components/charts/LineChartComp.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
-import { Box, Switch, Typography, TextField } from "@mui/material";
+import { Box, Switch, Typography, TextField, Slider } from "@mui/material";
 import { TableContext } from "../../../context/tableContext";
 import { useContext } from "react";
 
 export default function LineChartComp() {
-
   const [area, setArea] = useState(false);
 
-  const {data} = useContext(TableContext)
+  const { data } = useContext(TableContext);
 
   return (
     <>
       <Box>
         <LineChart
-          xAxis={[{ scaleType: 'point', data: data.rows[0] }]}
+          xAxis={[{ scaleType: "point", data: data.rows[0] }]}
           series={[
             {
               data: data.rows[1],
@@ -28,27 +27,47 @@ export default function LineChartComp() {
       <Box>
         <Switch
           checked={area}
-          onChange={() => {setArea(!area)}}
+          onChange={() => {
+            setArea(!area);
+          }}
           inputProps={{ "aria-label": "controlled" }}
         />
         <Typography>Area</Typography>
-      <Box>
-        <TextField
-          id="outlined-required"
-          label="Tag"
-          name="tagname"
-          sx={{ mt: 3 }}
-          value={data.tag}
+        <Slider
+          value={15}
+          valueLabelDisplay="auto"
+          min={1}
+          max={20}
+          aria-labelledby="input-item-number"
         />
-        <TextField
-          id="outlined-required"
-          label="template"
-          name="template"
-          sx={{ mt: 3 }}
-          value={data.template}
+        <Typography id="input-item-number" gutterBottom>
+          Number of items
+        </Typography>
+        <Slider
+          value={[3,17]}
+          min={1}
+          max={20}
         />
+        <Typography id="input-item-number" gutterBottom>
+          Scale
+        </Typography>
+        <Box>
+          <TextField
+            id="outlined-required"
+            label="Tag"
+            name="tagname"
+            sx={{ mt: 3 }}
+            value={data.tag}
+          />
+          <TextField
+            id="outlined-required"
+            label="template"
+            name="template"
+            sx={{ mt: 3 }}
+            value={data.template}
+          />
         </Box>
-        </Box>
+      </Box>
     </>
   );
 }
